a0c7fc40ceb17906f007278b615c7847d558760e
wolflergf <wolflergf@gmail.com>
2025-11-09 15:58:27 +0000

    End of the second day.

---
+++ phase-01-foundations/100-days-of-code/day-002/main.py
@@ -0,0 +1,8 @@
+# Day 2 Project: Tip Calculator
+
+print("Welcome to the tip calculator!")
+total_bill = float(input("What was the total bill? £"))
+tip = float(input("How much tip would you like to give? 10, 12, or 15%? £"))
+people_to_split = int(input("How many people to split the bill? "))
+calc = (total_bill * (1 + (tip / 100))) / people_to_split
+print(f"Each person should pay £{round(calc, 2)}")
